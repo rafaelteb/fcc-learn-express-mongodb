ea8851c731131c63eea791634cc766ae23bf20e9
Nerd Fish <nerdfish@mailbox.org>
2023-12-24 21:17:06 +0100

    add mongodb connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
@@ -50,6 +53,10 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+
+
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
